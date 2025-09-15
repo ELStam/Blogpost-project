03b8e5580da3c3a26cd36765a2f4ceec07c600e4
MilanSebes <milan@intermixmanagement.nl>
2025-09-15 11:41:41 +0200

    assets + app.vue aangepast

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <header>
-            <img src="../assets/writingwit.png" height="30" width="30"/>
+            <img src="/assets/writing-wit.png" height="30" width="30"/>
             <h1>Blog.</h1>
             <nav>
                 <router-link to="/">Home</router-link>
