2e13eeb98dc321fba8e93d814adade9d0648e8e7
MilanSebes <milan@intermixmanagement.nl>
2025-09-16 11:41:37 +0200

    scss georderd + profielfoto toegevoegd (nog niet werkend)

--- resources/js/components/ProfielComponent.vue
+++ resources/js/components/ProfielComponent.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="hero">
         <img
-            src="/assets/luca-bravo-O453M2Liufs-unsplash.jpg"
             alt="Header"
+            src="/assets/luca-bravo-O453M2Liufs-unsplash.jpg"
+        />
+    </div>
+
+    <div class="profilepicture">
+        <img
+            alt="profilepicture"
+            src="/assets/tyler-nix-PQeoQdkU9jQ-unsplash.jpg"
         />
     </div>
 </template>
