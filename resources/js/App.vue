a604f9519db47b6d3356e57e111c1258774ea3ed
developerintermix <esmee@intermixmanagement.nl>
2025-09-16 08:35:45 +0200

    Bug fix

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,10 +2,14 @@
     <div id="app">
         <header>
             <img src="/assets/writing-wit.png" height="30" width="30"/>
-            <h1>Blog.</h1>
+            <h1>BLOG.</h1>
             <nav>
                 <router-link to="/">Home</router-link>
                 <router-link :to="{name: 'UserList'}">Users</router-link>
+                <router-link to="/ontdek">Ontdek</router-link>
+                <router-link to="/volgend">Volgend</router-link>
+                <router-link to="/categorieen">Categorieën</router-link>
+                <router-link to="/TopPosts">Top Posts</router-link>
             </nav>
         </header>
         <main>
