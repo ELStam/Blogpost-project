36b048e6aa4fc9cea74b232781c79aef2f2e4e86
developerintermix <esmee@intermixmanagement.nl>
2025-09-17 11:21:08 +0200

    Blogs aanmaken en ophalen
    Mogelijk om een blog aan te maken en alle blogs op te halen in de backend

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +18,8 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::get('blogs', [BlogController::class, 'index'])->name('blogs.index');
+    Route::post('blogs', [BlogController::class, 'store'])->name('blogs.store');
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
