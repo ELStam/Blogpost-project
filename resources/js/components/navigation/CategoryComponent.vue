ef63fbad6a72efcdfca7fb6534e2fb81cee0d8a0
ELStam <esmee@intermixmanagement.nl>
2025-09-29 11:31:38 +0200

    Added category page (#41)

--- resources/js/components/navigation/CategoryComponent.vue
+++ resources/js/components/navigation/CategoryComponent.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="category">
-        <h3 class="category__label">{{ label }}</h3>
+        <div class="category-sidebar">
+            <h3 class="category__label">{{ label }}</h3>
+            <div class="category__list">
+                <ul class="category__list-items">
+                    <li class="category__list-item">
+                        <button class="category__button category__button--active">
+                            Alle blogs
+                        </button>
+                    </li>
+                    <li v-for="category in categories" class="category__list-item">
+                        <button class="category__button">{{ category.name }}</button>
+                    </li>
+                    <li class="category__list-item">
+                        <button class="category__button category__button--all">
+                            Bekijk alle categorieën
+                        </button>
+                    </li>
+                </ul>
+            </div>
 
-        <div class="category__list">
-            <ul class="category__list-items">
-                <li class="category__list-item">
-                    <button class="category__button category__button--active">
-                        Alle blogs
-                    </button>
-                </li>
-                <li v-for="category in categories" class="category__list-item">
-                    <button class="category__button">{{ category.name }}</button>
-                </li>
-                <li class="category__list-item">
-                    <button class="category__button category__button--all">
-                        Bekijk alle categorieën
-                    </button>
-                </li>
-            </ul>
         </div>
-
     </div>
 </template>
 
